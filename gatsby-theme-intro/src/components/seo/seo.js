32bfcead5b95911a14345636ebc9ea6b78d00b28
Ahmet Goch <ahmetgoch@yandex.ru>
2021-07-12 05:26:03 +0200

    add gtag

--- gatsby-theme-intro/src/components/seo/seo.js
+++ gatsby-theme-intro/src/components/seo/seo.js
@@ -32,7 +32,12 @@ const SEO = props => {
       script={[{ 
         type: 'text/javascript', 
         innerHTML: '(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2498526,hjsv:6};a=o.getElementsByTagName(\'head\')[0];r=o.createElement(\'script\');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,\'https://static.hotjar.com/c/hotjar-\',\'.js?sv=\');' 
-      }]}
+      },
+      {
+        type: 'text/javascript',
+        innerHTML: '<!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=G-6S8L82X1JQ"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag(\'js\', new Date());gtag(\'config\', \'G-6S8L82X1JQ\');</script>'
+      }
+      ]}
       title={title}
       meta={[
         {
