890db11f1735b366555cf031a339c1ff360ee50b
Ahmet Goch <ahmetgoch@yandex.ru>
2021-07-12 05:25:32 +0200

    add taxi web app and analytics

--- gatsby-theme-intro/src/components/seo/seo.js
+++ gatsby-theme-intro/src/components/seo/seo.js
@@ -29,6 +29,10 @@ const SEO = props => {
       htmlAttributes={{
         lang: site.siteMetadata.locale,
       }}
+      script={[{ 
+        type: 'text/javascript', 
+        innerHTML: '(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2498526,hjsv:6};a=o.getElementsByTagName(\'head\')[0];r=o.createElement(\'script\');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,\'https://static.hotjar.com/c/hotjar-\',\'.js?sv=\');' 
+      }]}
       title={title}
       meta={[
         {
